d1d678a945ad561c24cdd4c210426550f357c5bd
mateus <mateussilvamn18@gmail.com>
2026-07-04 19:37:53 -0300

    criando funcao de extrair dados

---
+++ etl.py
@@ -0,0 +1,11 @@
+import os, glob, pandas as pd
+
+def extrair_dados(pasta: str) -> pd.DataFrame:
+    arquivos_json = glob.glob(os.path.join(pasta, '*json'))
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    df_total = pd.concat(df_list, ignore_index=True)
+    return df_total
+
+
+
+
