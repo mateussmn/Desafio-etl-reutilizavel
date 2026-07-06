8924d7429a4a7fe48a81432e72307f9316998ce8
mateus <mateussilvamn18@gmail.com>
2026-07-05 21:33:38 -0300

    implementando log e timer

--- etl.py
+++ etl.py
@@ -1,15 +1,21 @@
 import os, glob, pandas as pd
+from log import log_decorator
+from timer import time_measure_decorator
 
+@log_decorator
 def extrair_dados(pasta: str) -> pd.DataFrame:
     arquivos_json = glob.glob(os.path.join(pasta, '*json'))
     df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
     df_total = pd.concat(df_list, ignore_index=True)
     return df_total
 
+@log_decorator 
+@time_measure_decorator
 def calcular_kpi_total_vendas(df: pd.DataFrame) -> pd.DataFrame:
     df["Total"] = df["Quantidade"] * df["Venda"]
     return df
 
+@log_decorator
 def carregar_dados(df: pd.DataFrame ,format_saida : list):
     for formato in format_saida:
         if formato == "csv":
@@ -19,6 +25,8 @@ def carregar_dados(df: pd.DataFrame ,format_saida : list):
 
     return
 
+@log_decorator
+@time_measure_decorator
 def pipeline_calculo_kpi(pasta :str, fortmat_saida :list):
     data_frame = extrair_dados(pasta)
     data_frame_total_calculado = calcular_kpi_total_vendas(data_frame)
